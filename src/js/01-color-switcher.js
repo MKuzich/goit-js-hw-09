35351d1e9d52257525c534ac263152c58d2de725
Mykhailo Kuzich <timmkedth@gmail.com>
2022-06-03 11:21:39 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]')
+}
 
+refs.btnStart.addEventListener('click', onStartHandler);
+refs.btnStop.addEventListener('click', onStopHandler);
+
+function onStartHandler(e) {
+    if (e.target.classList.contains('is-active')) {
+        return;
+    }
+    e.target.classList.add('is-active');
+    intervalId = setInterval(onBodyColorChange, 1000);
+}
+
+function onStopHandler() {
+    refs.btnStart.classList.remove('is-active');
+    clearInterval(intervalId);
+}
+
+function onBodyColorChange() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
